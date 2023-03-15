078b2ea6f53e3b53c7fc0d013fdbbd7d10b93a67
Giovanni <giovannibarolin@gmail.com>
2023-03-14 21:24:32 -0300

    Desafío 1

---
+++ Desafío-1/ProductManager.js
@@ -0,0 +1,80 @@
+class ProductManager{
+    #products;
+    #autoID=1;
+
+    constructor(){
+        this.#products=[];
+    } 
+    addProduct(product){
+        const {title,description,price,thumbnail,code,stock} = product;
+    
+        if(!title || !description || !price || !thumbnail || !code || !stock) throw new Error("Error, Todos los campos deben ser completados");
+        const productExist = this.#products.find(element=> element.code === product.code);
+        if(productExist) throw new Error("Error, Ya existe un producto con el mismo codigo");
+        const newProduct = {...product,id: this.#autoID};
+        this.#products.push(newProduct);
+        this.#autoID = this.#autoID + 1;
+    }
+    getProducts(){
+        return this.#products;
+    }
+    getProductById(productId){
+        const product = this.#products.find(element => element.id === productId);
+        if(!product) throw new Error("El producto no existe");
+        return product;
+    }
+
+}
+
+const productList=[
+    {
+        title:"Oreos",
+        description:"Galletitas rellenas",
+        price:23,
+        thumbnail:"sin foto",
+        code:"adfg66",
+        stock:15,
+    },
+    {
+        title:"Mana",
+        description:"Galletitas simples",
+        price:12,
+        thumbnail:"sin foto",
+        code:"adfg46",
+        stock:20,
+    },
+    {
+        title:"Pepitos",
+        description:"Galletitas con chips de chocolate",
+        price:22,
+        thumbnail:"sin foto",
+        code:"adfg13",
+        stock:15,
+    }
+]
+
+
+
+const galletitas = new ProductManager()
+console.log("Productos: ",galletitas.getProducts())
+
+try {
+    productList.forEach((product)=>galletitas.addProduct(product));
+} catch (error) {
+    console.log(error);
+}
+
+
+console.log("Productos: ",galletitas.getProducts());
+try {
+    console.log(galletitas.addProduct(productList[1]));
+} catch (error) {
+    console.log(error);
+}
+
+try {
+    console.log(galletitas.getProductById(1));
+    console.log(galletitas.getProductById(4));
+} catch (error) {
+    console.log(error);
+}
