a48a0f6dd14941ea1782387d0b704c4d7319a9dd
Giovanni <giovannibarolin@gmail.com>
2023-03-30 11:28:29 -0300

    Adding routes files

---
+++ Preentrega-1/routes/products.router.js
@@ -0,0 +1,43 @@
+import { Router } from "express";
+import ProductManager from "../src/ProductManager.js";
+import path from "path"
+
+const filePath = path.resolve("./src") + "/Products.json";
+const pm = new ProductManager(filePath);
+
+const routerProducts = Router();
+
+routerProducts.get('/',async(req,res)=>{
+    try {
+        const products = await pm.getProducts();
+        const limit = Number(req.query.limit) || products.length;
+        const productList = products.slice(0,limit);
+        return res.status(200).json({status:'success',message:productList});
+    } catch (error) {
+        res.status(500).send({status:"error",error:"No se pueden obtener los productos, error en el servidor"})
+    }  
+})
+
+routerProducts.get('/:pid',async (req,res)=>{
+    const productId = Number(req.params.pid);
+    if (typeof productId !== "number" || isNaN(productId)) {
+        res.status(400).send({status:"error",error: "El parámetro id debe ser un número válido" });
+        return;
+      }
+    try {
+        const product = await pm.getProductById(productId);
+        res.send(product);
+    } catch (error) {
+        console.log(error.message);
+        res.send({status:"error", error:error.message})
+    }
+        
+});
+
+routerProducts.post('/',async (req,res)=>{
+    const product = req.body;
+    console.log(product);
+        
+});
+
+export default routerProducts;
--- Preentrega-1/src/app.js
+++ Preentrega-1/src/app.js
@@ -1,9 +1,14 @@
 import Express from "express";
+import routerProducts from "../routes/products.router.js";
 
 const app = Express();
 const port = 8083;
 
 
+app.use(Express.json());
+app.use(Express.urlencoded({extended:true}));
+
+app.use('/api/products/',routerProducts);
 
 
 app.listen(port,()=>{
